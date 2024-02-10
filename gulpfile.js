2a6b79029f61413c25ede5f31ace1296cffb27d3
Plínio Pvv <gangss2@hotmail.com>
2024-02-10 14:01:42 -0300

    .

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ const { src, dest, series, parallel } = gulp;
  * VARIÁVEIS GLOBAIS
  */
 const ___NAME_FRONTEND = "frontend";
-const ___NAME_BACKEND = "backend"
+const ___NAME_BACKEND = "backend";
 
 const ___DIR_FRONTEND = `../${___NAME_FRONTEND}`;
 const ___DIR_BACKEND = `../${___NAME_BACKEND}`;
@@ -30,7 +30,7 @@ const ___DEST_SERVER_SIDE = "/app";
 async function compile_frontend(cb) {
   let res = execSync(`cd ${___DIR_FRONTEND} && ${___CMD_COMPILE_FRONTEND}`);
   // console.log(res.toString());
-  cb();
+  setTimeout(cb, 1000);
 }
 gulp.task("compile:frontend", compile_frontend);
 
@@ -40,7 +40,8 @@ async function compile_backend(cb) {
     `ncc build ${___DIR_BACKEND}/dist/main.js -o ${___DIR_BACKEND}/dist/ncc`
   );
   // console.log(res.toString());
-  cb();
+  console.log("####");
+  setTimeout(cb, 1000);
 }
 gulp.task("compile:backend", compile_backend);
 gulp.task("compile", parallel(compile_frontend, compile_backend));
@@ -116,8 +117,8 @@ async function deploy(cb) {
   var globs = [`${___DIR_PUBLISH}/**/*`, `${___DIR_PUBLISH}/backend/.env`];
 
   return await src(globs, { base: `${___DIR_PUBLISH}/`, buffer: false })
-  .pipe( conn.newer(`${___DEST_SERVER_SIDE}`))
-  .pipe(conn.dest(`${___DEST_SERVER_SIDE}`));
+    .pipe(conn.newer(`${___DEST_SERVER_SIDE}`))
+    .pipe(conn.dest(`${___DEST_SERVER_SIDE}`));
 }
 gulp.task("deploy", deploy);
 async function deploy_force(cb) {
@@ -132,18 +133,35 @@ async function deploy_force(cb) {
 
   var globs = [`${___DIR_PUBLISH}/**/*`, `${___DIR_PUBLISH}/backend/.env`];
 
-  return await src(globs, { base: `${___DIR_PUBLISH}/`, buffer: false })
-  .pipe(conn.dest(`${___DEST_SERVER_SIDE}`));
+  return await src(globs, { base: `${___DIR_PUBLISH}/`, buffer: false }).pipe(
+    conn.dest(`${___DEST_SERVER_SIDE}`)
+  );
 }
 gulp.task("deploy:force", deploy_force);
+
+/**
+ * TASK DE WAIT
+ */
+function wait(cb) {
+  setTimeout(cb, 1000);
+}
+gulp.task("wait1000", wait);
 
 /**
  * BUILD
  */
 // export const build = parallel("compile:frontend", "compile:backend");
+// export const build = series(
+//   series(clean_backend, clean_backend, publish_clean),
+//   "compile",
+//   series(publish_backend, publish_frontend)
+// );
 export const build = series(
-  parallel(clean_backend, clean_backend, publish_clean),
-  parallel("compile:frontend", "compile:backend"),
-  parallel(publish_backend, publish_frontend)
+  "clean",
+  "compile",
+  "wait1000",
+  "publish",
+  "wait1000",
+  "deploy"
 );
 export default build;
